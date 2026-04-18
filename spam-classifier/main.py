1d0079671c5f6b9c7fd18ec3a4d50d5e7fae332c
Madhuritha46 <madhuritha9406@gmail.com>
2026-04-18 23:01:30 +0530

    Add files via upload

---
+++ spam-classifier/main.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+
+print("Program started...")
+
+# Load dataset
+df = pd.read_csv("spam.csv", encoding='latin-1')
+
+df = df[['v1', 'v2']]
+df.columns = ['label', 'message']
+
+# Convert labels
+df['label'] = df['label'].map({'ham': 0, 'spam': 1})
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    df['message'], df['label'], test_size=0.2, random_state=42)
+
+# Convert text to numbers
+vectorizer = TfidfVectorizer()
+
+X_train_vec = vectorizer.fit_transform(X_train)
+X_test_vec = vectorizer.transform(X_test)
+
+# Train model
+model = MultinomialNB()
+model.fit(X_train_vec, y_train)
+
+# Accuracy
+y_pred = model.predict(X_test_vec)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+# Test input
+while True:
+    msg = input("\nEnter message: ")
+    msg_vec = vectorizer.transform([msg])
+    result = model.predict(msg_vec)
+
+    if result[0] == 1:
+        print("Spam ❌")
+    else:
+        print("Not Spam ✅")
